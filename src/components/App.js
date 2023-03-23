cf18239754503a8500192407dada2860ede35ce4
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-03-23 11:37:30 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-
+import { useState } from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+   const [inputValue, SetInputValue]=useState('');
+   const handleInputChange=(event)=>{
+    SetInputValue(event.target.value);
+   };
   return (
-    <div>
+    <div className="parent">
+      <Child inputValue={inputValue} handleInputChange={handleInputChange} />
         {/* Do not remove the main div */}
+    </div>
+  );
+};
+
+const Child =(props)=>{
+  return(
+    <div className="child">
+      <input type="text" value={props.inputValue}
+      onChange={props.handleInputChange}
+      />
     </div>
   )
 }
 
-export default App
+export default App;
